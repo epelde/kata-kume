7e1106ea2c3151168bc2c43891b66a6a535584dc
aepelde <aepelde@theinit.com>
2019-07-08 09:03:13 +0200

    project structure created

---
+++ src/main/java/io/github/epelde/katakume/App.java
@@ -0,0 +1,13 @@
+package io.github.epelde.katakume;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
