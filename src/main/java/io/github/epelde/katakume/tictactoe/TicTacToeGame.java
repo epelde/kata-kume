efa1ef90ad7ecd741c34fa6bc6ca742e3d3d4a24
epelde <aepelde@gmail.com>
2019-07-08 14:08:34 +0200

    the game is not over when started

---
+++ src/main/java/io/github/epelde/katakume/tictactoe/TicTacToeGame.java
@@ -0,0 +1,10 @@
+package io.github.epelde.katakume.tictactoe;
+
+public class TicTacToeGame {
+
+    private boolean over = false;
+
+    public boolean isOver() {
+        return over;
+    }
+}
--- src/test/java/io/github/epelde/katakume/tictactoe/TicTacToeTests.java
+++ src/test/java/io/github/epelde/katakume/tictactoe/TicTacToeTests.java
@@ -2,12 +2,13 @@ package io.github.epelde.katakume.tictactoe;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TicTacToeTests {
 
     @Test
-    public void dumbTest() {
-        assertTrue(true);
+    public void gameIsNotOverWhenStarted() {
+        TicTacToeGame game = new TicTacToeGame();
+        assertFalse(game.isOver());
     }
 }
