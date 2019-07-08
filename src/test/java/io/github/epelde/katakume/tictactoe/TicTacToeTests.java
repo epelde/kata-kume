38f6a3dff0ea8c48e4d3d0ed7522510b6c011292
epelde <aepelde@gmail.com>
2019-07-08 22:13:58 +0200

    set game as class variable

--- src/test/java/io/github/epelde/katakume/tictactoe/TicTacToeTests.java
+++ src/test/java/io/github/epelde/katakume/tictactoe/TicTacToeTests.java
@@ -1,27 +1,32 @@
 package io.github.epelde.katakume.tictactoe;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TicTacToeTests {
 
+    private TicTacToeGame game;
+
+    @BeforeEach
+    public void setUp() {
+        game = new TicTacToeGame();
+    }
+
     @Test
     public void A_game_is_not_over_when_started() {
-        TicTacToeGame game = new TicTacToeGame();
         assertFalse(game.isOver());
     }
 
     @Test
     public void A_player_can_take_a_field_if_not_already_taken() throws FieldAlreadyTakenException {
-        TicTacToeGame game = new TicTacToeGame();
         game.play(new Field(1, 1));
         game.play(new Field(1, 2));
     }
 
     @Test
     public void A_player_can_not_take_an_already_taken_field() throws FieldAlreadyTakenException {
-        TicTacToeGame game = new TicTacToeGame();
         game.play(new Field(1,1));
         try {
             game.play(new Field(1,1));
