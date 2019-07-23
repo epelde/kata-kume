33a8a31bea19454aeb92f20d0f500c832635b74f
epelde <aepelde@gmail.com>
2019-07-23 17:10:13 +0200

    improve test cases

--- src/test/java/io/github/epelde/katakume/leapyear/LeapYearTests.java
+++ src/test/java/io/github/epelde/katakume/leapyear/LeapYearTests.java
@@ -15,42 +15,40 @@ public class LeapYearTests {
         leapYear = new LeapYear();
     }
 
-    // All years divisible by 400 ARE leap years
-    // (so, for example, 2000 was indeed a leap year),
     @Test
-    public void aYearDivisibleBy400ShouldBeLeap() {
+    public void aYearNotDivisibleBy4IsACommonYear() {
+        assertFalse(leapYear.isLeap(2001));
+        assertFalse(leapYear.isLeap(2003));
+        assertFalse(leapYear.isLeap(2005));
+        assertFalse(leapYear.isLeap(2006));
+        assertFalse(leapYear.isLeap(2017));
+        assertFalse(leapYear.isLeap(2018));
+        assertFalse(leapYear.isLeap(2019));
+    }
+
+    @Test
+    public void aYearDivisibleBy4ButNotBy100IsLeapYear() {
+        assertTrue(leapYear.isLeap(2008));
+        assertTrue(leapYear.isLeap(2012));
+        assertTrue(leapYear.isLeap(2016));
+        assertTrue(leapYear.isLeap(1988));
+        assertTrue(leapYear.isLeap(1992));
+    }
+
+    @Test
+    public void aYearDivisibleBy4AndBy100IsLeapCommon() {
+        assertFalse(leapYear.isLeap(1900));
+        assertFalse(leapYear.isLeap(2100));
+        assertFalse(leapYear.isLeap(2100));
+        assertFalse(leapYear.isLeap(2200));
+    }
+
+    @Test
+    public void aYearDivisibleBy400IsALeapYear() {
+        assertTrue(leapYear.isLeap(1600));
         assertTrue(leapYear.isLeap(2000));
         assertTrue(leapYear.isLeap(2400));
         assertTrue(leapYear.isLeap(2800));
     }
 
-    // All years divisible by 4 but not by 100 ARE leap years
-    // (e.g., 2008, 2012, 2016),
-    @Test
-    public void aYearDivisibleBy4ButNotBy100IsLeap() {
-        assertTrue(leapYear.isLeap(2008));
-        assertTrue(leapYear.isLeap(2012));
-        assertTrue(leapYear.isLeap(2016));
-        assertTrue(leapYear.isLeap(1988));
-        assertFalse(leapYear.isLeap(2200));
-    }
-
-    // All years divisible by 100 but not by 400 are NOT leap years
-    // (so, for example, 1700, 1800, and 1900 were NOT leap years, NOR will 2100 be a leap year),
-    @Test
-    public void aYearDivisibleBy100ButNotBy400AIsNOTYearLeap() {
-        assertFalse(leapYear.isLeap(1700));
-        assertFalse(leapYear.isLeap(1800));
-        assertFalse(leapYear.isLeap(1900));
-        assertFalse(leapYear.isLeap(2100));
-    }
-
-    // All years not divisible by 4 are NOT leap years
-    // (e.g. 2017, 2018, 2019).
-    @Test
-    public void aYearNotDivisibleBy4IsNOTLeapYear() {
-        assertFalse(leapYear.isLeap(2017));
-        assertFalse(leapYear.isLeap(2018));
-        assertFalse(leapYear.isLeap(2019));
-    }
 }
