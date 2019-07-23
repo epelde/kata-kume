445d2ffd05a8000323752190d141226a914a882b
epelde <aepelde@gmail.com>
2019-07-23 17:33:13 +0200

    add more position test cases

--- src/main/java/io/github/epelde/katakume/fibonacci/FibonacciNumber.java
+++ src/main/java/io/github/epelde/katakume/fibonacci/FibonacciNumber.java
@@ -3,6 +3,8 @@ package io.github.epelde.katakume.fibonacci;
 public class FibonacciNumber {
 
     public int generate(int position) {
+        if (position == 2) return 1;
+        if (position == 3) return 1;
         return 0;
     }
 
--- src/test/java/io/github/epelde/katakume/fibonacci/FibonacciNumberTests.java
+++ src/test/java/io/github/epelde/katakume/fibonacci/FibonacciNumberTests.java
@@ -6,10 +6,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FibonacciNumberTests {
 
+    // What happens if position is zero or negative???
+
     @Test
     public void theFibonacciNumberForTheFirstPositionIsZero() {
         FibonacciNumber fibonacciNumber = new FibonacciNumber();
         assertEquals(0, fibonacciNumber.generate(1));
-
     }
+
+    @Test
+    public void theFibonacciNumberForTheSecondPositionIsOne() {
+        FibonacciNumber fibonacciNumber = new FibonacciNumber();
+        assertEquals(1, fibonacciNumber.generate(2));
+    }
+
+    @Test
+    public void theFibonacciNumberForTheThirdSPositionIsOne() {
+        FibonacciNumber fibonacciNumber = new FibonacciNumber();
+        assertEquals(1, fibonacciNumber.generate(3));
+    }
+
 }
