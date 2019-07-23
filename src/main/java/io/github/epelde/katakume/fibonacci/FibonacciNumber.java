4d287de25bedbde0fcb2aa10bab7d29e8d8c6f0e
epelde <aepelde@gmail.com>
2019-07-23 17:40:27 +0200

    improve tests expresiveness

--- src/main/java/io/github/epelde/katakume/fibonacci/FibonacciNumber.java
+++ src/main/java/io/github/epelde/katakume/fibonacci/FibonacciNumber.java
@@ -5,6 +5,7 @@ public class FibonacciNumber {
     public int generate(int position) {
         if (position == 2) return 1;
         if (position == 3) return 1;
+        if (position == 4) return 2;
         return 0;
     }
 
--- src/test/java/io/github/epelde/katakume/fibonacci/FibonacciNumberTests.java
+++ src/test/java/io/github/epelde/katakume/fibonacci/FibonacciNumberTests.java
@@ -1,29 +1,40 @@
 package io.github.epelde.katakume.fibonacci;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class FibonacciNumberTests {
+
+    private FibonacciNumber fibonacciNumber;
+
+    @BeforeEach
+    public void setup() {
+        fibonacciNumber = new FibonacciNumber();
+    }
 
     // What happens if position is zero or negative???
 
     @Test
     public void theFibonacciNumberForTheFirstPositionIsZero() {
-        FibonacciNumber fibonacciNumber = new FibonacciNumber();
-        assertEquals(0, fibonacciNumber.generate(1));
+        assertThat(fibonacciNumber.generate(1), equalTo(0));
     }
 
     @Test
     public void theFibonacciNumberForTheSecondPositionIsOne() {
-        FibonacciNumber fibonacciNumber = new FibonacciNumber();
-        assertEquals(1, fibonacciNumber.generate(2));
+        assertThat(fibonacciNumber.generate(2), equalTo(1));
     }
 
     @Test
-    public void theFibonacciNumberForTheThirdSPositionIsOne() {
-        FibonacciNumber fibonacciNumber = new FibonacciNumber();
-        assertEquals(1, fibonacciNumber.generate(3));
+    public void theFibonacciNumberForTheThirdPositionIsOne() {
+        assertThat(fibonacciNumber.generate(3), equalTo(1));
+    }
+
+    @Test
+    public void theFibonacciNumberForTheFourthPositionIsTwo() {
+        assertThat(fibonacciNumber.generate(4), equalTo(2));
     }
 
 }
