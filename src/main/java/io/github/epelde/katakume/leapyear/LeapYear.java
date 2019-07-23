c93d83050bdc2edab9718456f02fd5f5e5399968
epelde <aepelde@gmail.com>
2019-07-23 14:45:45 +0200

    leap year kata solution

---
+++ src/main/java/io/github/epelde/katakume/leapyear/LeapYear.java
@@ -0,0 +1,19 @@
+package io.github.epelde.katakume.leapyear;
+
+public class LeapYear {
+    public boolean isLeap(int year) {
+        return isDivisibleBy400(year) || (isDivisibleBy4(year) && !isDivisibleBy100(year));
+    }
+
+    private boolean isDivisibleBy400(int year) {
+        return year % 400 == 0;
+    }
+
+    private boolean isDivisibleBy4(int year) {
+        return year % 4 == 0;
+    }
+
+    private boolean isDivisibleBy100(int year) {
+        return year % 100 == 0;
+    }
+}
