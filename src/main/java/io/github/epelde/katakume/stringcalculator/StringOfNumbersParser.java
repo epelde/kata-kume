47e9ba643d369921a59805799add2ca86eaa12a0
epelde <aepelde@gmail.com>
2019-08-01 21:42:04 +0200

    parse input string argument and remove constructor

--- src/main/java/io/github/epelde/katakume/stringcalculator/StringOfNumbersParser.java
+++ src/main/java/io/github/epelde/katakume/stringcalculator/StringOfNumbersParser.java
@@ -7,19 +7,13 @@ public class StringOfNumbersParser {
 
     private final String DEFAULT_DELIMITER = ",|\n";
 
-    private String input;
+    public BigDecimal[] parse(String input) {
 
-    StringOfNumbersParser(String input) {
-        this.input = input;
-    }
-
-    public BigDecimal[] parse() {
-
-        if (this.input.isEmpty()) {
+        if (input.isEmpty()) {
             return new BigDecimal[]{ new BigDecimal("0") };
         }
 
-        return Arrays.stream(this.input.split(DEFAULT_DELIMITER))
+        return Arrays.stream(input.split(DEFAULT_DELIMITER))
                 .map(number -> new BigDecimal(number))
                 .toArray(BigDecimal[]::new);
     }
