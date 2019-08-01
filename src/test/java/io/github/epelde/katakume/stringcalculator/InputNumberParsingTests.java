d08f771e42561e8e232154471475c91b39e492ec
epelde <aepelde@gmail.com>
2019-08-01 14:09:02 +0200

    improve two decimal number parsing test case

--- src/test/java/io/github/epelde/katakume/stringcalculator/InputNumberParsingTests.java
+++ src/test/java/io/github/epelde/katakume/stringcalculator/InputNumberParsingTests.java
@@ -48,4 +48,14 @@ public class InputNumberParsingTests {
         assertThat(output[1], equalTo("2"));
     }
 
+    @Test
+    public void string_containing_two_decimal_numbers_returns_array_with_two_numbers() {
+        String[] output = parser.parse("1.1,2.2");
+
+        assertThat(output.length, equalTo(2));
+        assertThat(output[0], equalTo("1.1"));
+        assertThat(output[1], equalTo("2.2"));
+    }
+
+
 }
