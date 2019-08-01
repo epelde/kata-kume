253332d0ec78791afc7dab4d76b3a49372368204
epelde <aepelde@gmail.com>
2019-08-01 18:08:43 +0200

    parser returns big decimal array

--- src/main/java/io/github/epelde/katakume/stringcalculator/StringOfNumbersParser.java
+++ src/main/java/io/github/epelde/katakume/stringcalculator/StringOfNumbersParser.java
@@ -1,5 +1,6 @@
 package io.github.epelde.katakume.stringcalculator;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 public class StringOfNumbersParser {
@@ -10,11 +11,15 @@ public class StringOfNumbersParser {
         this.delimiter = Arrays.stream(delimiter).reduce((sep1, sep2) -> sep1 + "|" + sep2).orElse(",");
     }
 
-    public String[] parse(String input) {
+    public BigDecimal[] parse(String input) {
+
         if (input.isEmpty()) {
-            return new String[] { "0" };
+            return new BigDecimal[]{ new BigDecimal("0") };
         }
-        return input.split(delimiter);
+
+        return Arrays.stream(input.split(delimiter))
+                .map(number -> new BigDecimal(number))
+                .toArray(BigDecimal[]::new);
     }
 
 }
