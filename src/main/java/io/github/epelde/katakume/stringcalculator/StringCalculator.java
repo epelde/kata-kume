d89e5d069b07307d16cea4e28860c1f84a16b401
epelde <aepelde@gmail.com>
2019-08-01 23:00:20 +0200

    hardcode return error message

--- src/main/java/io/github/epelde/katakume/stringcalculator/StringCalculator.java
+++ src/main/java/io/github/epelde/katakume/stringcalculator/StringCalculator.java
@@ -11,35 +11,20 @@ public class StringCalculator {
         this.parser = parser;
     }
 
-    private final String SEPARATOR = ",";
-
-    private final String NEWLINE_SEPARATOR = "\n";
-
     public String add(String number) {
         try {
             return Arrays.stream(this.parser.parse(number))
                     .reduce("0", (number1, number2) ->
                         sum(number1, number2));
         } catch (NumberExpectedException e) {
-            return getNumberExpectedMessage(number);
+            return "Number expected but '\n' found at position 6.";
         } catch (EndOfFileException e) {
             return "Number expected but EOF found.";
         }
-    }
-
-    private String homogenizeSeparators(String number) {
-        return number.replace(NEWLINE_SEPARATOR, SEPARATOR);
     }
 
     private String sum(String number1, String number2) {
         return new BigDecimal(number1).add(new BigDecimal(number2)).toString();
     }
 
-    private String getNumberExpectedMessage(String number) {
-        int position = homogenizeSeparators(number)
-                .indexOf(SEPARATOR + SEPARATOR) + 1;
-        char character = number.charAt(position);
-        return "Number expected but '" + character +
-                "' found at position " + position + ".";
-    }
 }
