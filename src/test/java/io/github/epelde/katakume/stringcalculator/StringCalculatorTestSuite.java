eda6ce1214cfd3f4e4b0c4963d832155c5445870
epelde <aepelde@gmail.com>
2019-08-01 22:49:24 +0200

    add kata test suite

---
+++ src/test/java/io/github/epelde/katakume/stringcalculator/StringCalculatorTestSuite.java
@@ -0,0 +1,10 @@
+package io.github.epelde.katakume.stringcalculator;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("io.github.epelde.katakume.stringcalculator")
+public class StringCalculatorTestSuite {
+}
