5b945013b98008ab45d379e46a6e4884135c20bd
epelde <aepelde@gmail.com>
2019-08-01 14:02:22 +0200

    parse string of number with just one number

--- src/main/java/io/github/epelde/katakume/stringcalculator/InputNumberParser.java
+++ src/main/java/io/github/epelde/katakume/stringcalculator/InputNumberParser.java
@@ -3,6 +3,9 @@ package io.github.epelde.katakume.stringcalculator;
 public class InputNumberParser {
 
     public String[] parse(String input) {
+        if (!input.isEmpty()) {
+            return new String[] { input };
+        }
         return new String[] { "0" };
     }
 
--- src/test/java/io/github/epelde/katakume/stringcalculator/InputNumberParsingTests.java
+++ src/test/java/io/github/epelde/katakume/stringcalculator/InputNumberParsingTests.java
@@ -22,4 +22,13 @@ public class InputNumberParsingTests {
         assertThat(output.length, equalTo(1));
         assertThat(output[0], equalTo("0"));
     }
+
+    @Test
+    public void string_containing_just_one_number_returns_array_with_the_number() {
+        String[] output = parser.parse("1");
+
+        assertThat(output.length, equalTo(1));
+        assertThat(output[0], equalTo("1"));
+
+    }
 }
