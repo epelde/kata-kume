09fc52f6cb001be5ba949163b9f8f3a291d3643c
epelde <aepelde@gmail.com>
2019-08-01 09:23:30 +0200

    remove unnecessary code

--- src/main/java/io/github/epelde/katakume/stringcalculator/StringCalculator.java
+++ src/main/java/io/github/epelde/katakume/stringcalculator/StringCalculator.java
@@ -10,24 +10,11 @@ public class StringCalculator {
     private final String NEWLINE_SEPARATOR = "\n";
 
     public String add(String number) {
-
-        if (number.isEmpty()) {
-            return "0";
-        }
-
-        if (containsJustOneNumber(number)) {
-            return number;
-        }
-
         return Arrays.stream(number.replace(NEWLINE_SEPARATOR, SEPARATOR)
                 .split(SEPARATOR))
-                .reduce("", (number1, number2) ->
+                .reduce("0", (number1, number2) ->
                         stringToNumber(number1).add(stringToNumber(number2)).toString()
                 );
-    }
-
-    private boolean containsJustOneNumber(String number) {
-        return !number.contains(SEPARATOR);
     }
 
     private BigDecimal stringToNumber(String number) {
