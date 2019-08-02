9e7202e530746014e12839a81e4916deb4f01e53
epelde <aepelde@gmail.com>
2019-08-02 13:22:01 +0200

    any three different books get another greater discount

--- src/test/java/io/github/epelde/katakume/potter/BasketOfBooksTotalPriceTests.java
+++ src/test/java/io/github/epelde/katakume/potter/BasketOfBooksTotalPriceTests.java
@@ -2,8 +2,7 @@ package io.github.epelde.katakume.potter;
 
 import org.junit.jupiter.api.Test;
 
-import static io.github.epelde.katakume.potter.BasketOfBooksTotalPriceTests.Book.BOOK1;
-import static io.github.epelde.katakume.potter.BasketOfBooksTotalPriceTests.Book.BOOK2;
+import static io.github.epelde.katakume.potter.BasketOfBooksTotalPriceTests.Book.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BasketOfBooksTotalPriceTests {
@@ -36,6 +35,14 @@ public class BasketOfBooksTotalPriceTests {
         assertTrue(15.2 == basket.price());
     }
 
+    @Test
+    public void any_three_different_books_get_a_discount() {
+        Basket basket = new Basket(new Book[] { BOOK1, BOOK2, BOOK3 });
+
+        assertTrue(21.6 == basket.price());
+    }
+
+
     public enum Book {
         BOOK1, BOOK2, BOOK3, BOOK4, BOOK5
     }
@@ -43,6 +50,7 @@ public class BasketOfBooksTotalPriceTests {
     private class Basket {
 
         private static final double TWO_DIFFERENT_DISCOUNT = 0.95;
+        private static final double THREE_DIFFERENT_DISCOUNT = 0.90;
 
         private Book[] books;
 
@@ -53,6 +61,10 @@ public class BasketOfBooksTotalPriceTests {
         public double price() {
             if (this.books.length == 2 && this.books[0] != this.books[1]) {
                 return 8 * this.books.length * TWO_DIFFERENT_DISCOUNT;
+            }
+            if (this.books.length == 3 && this.books[0] != this.books[1] && this.books[0] != this.books[2]
+                && this.books[1] != this.books[2]) {
+                return 8 * this.books.length * THREE_DIFFERENT_DISCOUNT;
             }
             return 8 * this.books.length;
         }
