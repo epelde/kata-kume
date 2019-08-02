6fdd8789cced7279ce81bee65a5e35f3624edcd1
epelde <aepelde@gmail.com>
2019-08-02 13:02:53 +0200

    any two different books get a discount indeed

--- src/test/java/io/github/epelde/katakume/potter/BasketOfBooksTotalPriceTests.java
+++ src/test/java/io/github/epelde/katakume/potter/BasketOfBooksTotalPriceTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static io.github.epelde.katakume.potter.BasketOfBooksTotalPriceTests.Book.BOOK1;
 import static io.github.epelde.katakume.potter.BasketOfBooksTotalPriceTests.Book.BOOK2;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BasketOfBooksTotalPriceTests {
 
@@ -12,21 +12,28 @@ public class BasketOfBooksTotalPriceTests {
     public void empty_basket_price_is_zero() {
         Basket basket = new Basket(new Book[] {});
 
-        assertEquals(0, basket.price());
+        assertTrue(0 == basket.price());
     }
 
     @Test
     public void one_copy_of_any_of_books_costs_8_euros() {
         Basket basket = new Basket(new Book[] { BOOK1 });
 
-        assertEquals(8, basket.price());
+        assertTrue(8 == basket.price());
     }
 
     @Test
     public void copies_of_any_same_book_cost_8_euros_each() {
         Basket basket = new Basket(new Book[] { BOOK1, BOOK1, BOOK1, BOOK1, BOOK1 });
 
-        assertEquals(40, basket.price());
+        assertTrue(40 == basket.price());
+    }
+
+    @Test
+    public void any_two_different_books_get_a_discount() {
+        Basket basket = new Basket(new Book[] { BOOK1, BOOK2 });
+
+        assertTrue(15.2 == basket.price());
     }
 
     public enum Book {
@@ -41,7 +48,10 @@ public class BasketOfBooksTotalPriceTests {
             this.books = books;
         }
 
-        public int price() {
+        public double price() {
+            if (this.books.length == 2 && this.books[0] != this.books[1]) {
+                return 8 * this.books.length * 0.95;
+            }
             return 8 * this.books.length;
         }
     }
