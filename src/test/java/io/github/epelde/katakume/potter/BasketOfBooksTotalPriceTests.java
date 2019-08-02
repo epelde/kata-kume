9ee8a6f98f2ef20fc6c740747c87545f86a93d2f
epelde <aepelde@gmail.com>
2019-08-02 11:20:24 +0200

    book as a type of data

--- src/test/java/io/github/epelde/katakume/potter/BasketOfBooksTotalPriceTests.java
+++ src/test/java/io/github/epelde/katakume/potter/BasketOfBooksTotalPriceTests.java
@@ -2,36 +2,42 @@ package io.github.epelde.katakume.potter;
 
 import org.junit.jupiter.api.Test;
 
+import static io.github.epelde.katakume.potter.BasketOfBooksTotalPriceTests.Book.BOOK1;
+import static io.github.epelde.katakume.potter.BasketOfBooksTotalPriceTests.Book.BOOK2;
 import static org.junit.Assert.assertEquals;
 
 public class BasketOfBooksTotalPriceTests {
 
     @Test
     public void empty_basket_price_is_zero() {
-        Basket basket = new Basket(new String[] {});
+        Basket basket = new Basket(new Book[] {});
 
         assertEquals(0, basket.price());
     }
 
     @Test
     public void one_copy_of_any_of_books_costs_8_euros() {
-        Basket basket = new Basket(new String[] { "BOOK1" });
+        Basket basket = new Basket(new Book[] { BOOK1 });
 
         assertEquals(8, basket.price());
     }
 
     @Test
-    public void two_copies_of_any_of_books_costs_8_euros_each() {
-        Basket basket = new Basket(new String[] { "BOOK1", "BOOK2" });
+    public void two_copies_of_any_of_books_cost_8_euros_each() {
+        Basket basket = new Basket(new Book[] { BOOK1, BOOK2 });
 
         assertEquals(16, basket.price());
     }
 
+    public enum Book {
+        BOOK1, BOOK2, BOOK3, BOOK4, BOOK5
+    }
+
     private class Basket {
 
-        private String[] books;
+        private Book[] books;
 
-        public Basket(String[] books) {
+        public Basket(Book[] books) {
             this.books = books;
         }
 
