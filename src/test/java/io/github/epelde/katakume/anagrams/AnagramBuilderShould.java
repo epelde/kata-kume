5a5c02b5d374466bbd3655b7362ca77e7e3a6d8a
epelde <aepelde@gmail.com>
2019-09-17 16:11:43 +0200

    add set up section to tests

--- src/test/java/io/github/epelde/katakume/anagrams/AnagramBuilderShould.java
+++ src/test/java/io/github/epelde/katakume/anagrams/AnagramBuilderShould.java
@@ -1,21 +1,27 @@
 package io.github.epelde.katakume.anagrams;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class AnagramBuilderShould {
 
+    private AnagramBuilder builder;
+
+    @BeforeEach
+    void setup() {
+        builder = new AnagramBuilder();
+    }
+
     @Test
     void returns_anagram_combination_for_1_char() {
-        AnagramBuilder builder = new AnagramBuilder();
         assertArrayEquals(new String[] { "b" }, builder.build("b"));
         assertArrayEquals(new String[] { "a" }, builder.build("a"));
     }
 
     @Test
     void returns_anagram_combination_for_2_chars() {
-        AnagramBuilder builder = new AnagramBuilder();
         assertArrayEquals(new String[] { "ab", "ba" }, builder.build("ab"));
         assertArrayEquals(new String[] { "ac", "ca" }, builder.build("ac"));
     }
