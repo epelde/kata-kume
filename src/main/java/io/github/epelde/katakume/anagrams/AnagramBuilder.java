60cf78812740533e186035537a712039fa7b9043
epelde <aepelde@gmail.com>
2019-09-17 15:12:46 +0200

    Simple faked anagram combination

---
+++ src/main/java/io/github/epelde/katakume/anagrams/AnagramBuilder.java
@@ -0,0 +1,11 @@
+package io.github.epelde.katakume.anagrams;
+
+public class AnagramBuilder {
+
+
+    public String[] convert(String anagram) {
+
+        return new String[] {"b"};
+
+    }
+}
---
+++ src/test/java/io/github/epelde/katakume/anagrams/CocoteroShould.java
@@ -0,0 +1,13 @@
+package io.github.epelde.katakume.anagrams;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CocoteroShould {
+
+    @Test
+    void returns_anagram_combination_for_1_char() {
+        AnagramBuilder anagramB = new AnagramBuilder();
+        Assertions.assertArrayEquals(new String[] {"b"}, anagramB.convert("b"));
+    }
+}
