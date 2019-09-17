6b15769e202781eea0b2e35edb7606ebd5a4bb8f
epelde <aepelde@gmail.com>
2019-09-17 16:08:45 +0200

    rename variable

--- src/test/java/io/github/epelde/katakume/anagrams/AnagramBuilderShould.java
+++ src/test/java/io/github/epelde/katakume/anagrams/AnagramBuilderShould.java
@@ -8,16 +8,16 @@ public class AnagramBuilderShould {
 
     @Test
     void returns_anagram_combination_for_1_char() {
-        AnagramBuilder anagramB = new AnagramBuilder();
-        assertArrayEquals(new String[] { "b" }, anagramB.build("b"));
-        assertArrayEquals(new String[] { "a" }, anagramB.build("a"));
+        AnagramBuilder builder = new AnagramBuilder();
+        assertArrayEquals(new String[] { "b" }, builder.build("b"));
+        assertArrayEquals(new String[] { "a" }, builder.build("a"));
     }
 
     @Test
     void returns_anagram_combination_for_2_chars() {
-        AnagramBuilder anagramB = new AnagramBuilder();
-        assertArrayEquals(new String[] { "ab", "ba" }, anagramB.build("ab"));
-        assertArrayEquals(new String[] { "ac", "ca" }, anagramB.build("ac"));
+        AnagramBuilder builder = new AnagramBuilder();
+        assertArrayEquals(new String[] { "ab", "ba" }, builder.build("ab"));
+        assertArrayEquals(new String[] { "ac", "ca" }, builder.build("ac"));
     }
 
 }
